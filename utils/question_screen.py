131b260e842cae3cd52a984c391c52f16f18a508
Stephen Adewale <adewalestephennifemi@gmail.com>
2026-09-03 13:44:10 +0100

    question screen is currently static. The timer doesn't update dynamically just yet

--- utils/question_screen.py
+++ utils/question_screen.py
@@ -1,7 +1,7 @@
-def question(question, time_left, q_num, total_q, options):
+def question(question, time_left, q_num, total_q, options, current_answer=None):
     width = 60
     
-    header_text = f"QUESTION: {q_num}/{total_q} | TIME LEFT: {time_left}"
+    header_text = f"QUESTION: {q_num}/{total_q} | TIME LEFT: {time_left*60} seconds"
     
     # 1. Print Top Border and Header
     print("=" * width)
@@ -13,16 +13,21 @@ def question(question, time_left, q_num, total_q, options):
     print(f"QUESTION {q_num}\n{question}")
     print()
 
-    # 3. SHowing options
+    # 3. Showing options
     for option in options:
         print(f"{option}")
     print()
     
-    # 4. Print Bottom Navigation and Borders
+    # 4. Show current answer if one exists
+    if current_answer is not None:
+        print(f"[Current Answer: {current_answer}]".center(width))
+        print()
+    
+    # 5. Print Bottom Navigation and Borders
     print("=" * width)
-    print("[P]revious  |  [N]ext  |  [S]ubmit  |  [1-4]Answer".center(width))
+    print("[P]revious  |  [N]ext  |  [S]ubmit  |  [A-D]Answer".center(width))
     print("=" * width)
     
-    # 5. Take User Input at the very bottom
+    # 6. Take User Input at the very bottom
     answer = input("\n Your Answer (or action): ")
     return answer
